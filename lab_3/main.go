0025a53dd2f840a5cd70cacf91b6dece24aeb959
jamesmaxwell13 <jamesclerkmaxwell13@gmail.com>
2024-11-17 13:43:54 +0300

    add readmes and lab3

---
+++ lab_3/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"errors"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/theme"
+	"go.bug.st/serial"
+	"lab_3/gui"
+	"lab_3/packet"
+	"lab_3/rs232"
+	"sync"
+	"time"
+)
+
+func TransmitData(u *gui.UserInterface) {
+	prevText := ""
+	for {
+		if u.InputEntry != nil && u.InputPort.SerialPort != nil && u.InputPort.Number != 0 {
+			currentText := u.InputEntry.Text
+			for len(currentText)-len(prevText) >= 7 {
+				rawPacket, formattedPacket, err := packet.SerializePacket(
+					currentText[len(prevText):len(prevText)+7],
+					u.InputPort.Number,
+				)
+				if err != nil {
+					gui.ErrorWindow(err, u.App)
+				}
+				err = u.InputPort.WriteBytes(rawPacket)
+				if err != nil {
+					gui.ErrorWindow(err, u.App)
+				}
+				u.TransmittedBytes += 7
+				u.UpdateStatus(formattedPacket)
+				prevText = currentText[:len(prevText)+7]
+			}
+		}
+	}
+}
+
+func ReceiveData(u *gui.UserInterface, mutex *sync.Mutex) {
+	for {
+		mutex.Lock()
+		if u.OutputEntry != nil && u.OutputPort.SerialPort != nil {
+			rawData, err := u.OutputPort.ReadBytes()
+			mutex.Unlock()
+			if err != nil && err.Error() != "Port has been closed" {
+				gui.ErrorWindow(err, u.App)
+				continue
+			}
+			data, errPacket := packet.ParseRawData(rawData)
+			if errPacket != nil {
+				gui.ErrorWindow(errPacket, u.App)
+			}
+			u.OutputEntry.SetText(u.OutputEntry.Text + data)
+		} else {
+			mutex.Unlock()
+		}
+	}
+}
+
+func main() {
+	u := new(gui.UserInterface)
+	u.App = app.New()
+	w := u.App.NewWindow("Serial port communication")
+	u.App.Settings().SetTheme(&gui.СustomTheme{Theme: theme.DefaultTheme()})
+	ports, err := serial.GetPortsList()
+	if err != nil || len(ports) == 0 {
+		gui.ErrorWindow(errors.New("No serial ports found"), u.App)
+		time.Sleep(time.Minute)
+		panic("No serial ports found!")
+	}
+	u.InputPort = new(rs232.Port)
+	u.OutputPort = new(rs232.Port)
+	u.TransmittedBytes = 0
+	u.InitEntries()
+	u.InitSelects(ports)
+	u.UpdateStatus("")
+	u.MakeGrid()
+	w.SetContent(u.Grid)
+	w.Resize(fyne.NewSize(1200, 400))
+	//(675, 475)
+	go func() {
+		for {
+			if u.InputPort.SerialPort == nil || u.OutputPort.SerialPort == nil {
+				err := gui.UpdatePorts(u.SelectInputPort, u.SelectOutputPort)
+				if err != nil {
+					gui.ErrorWindow(err, u.App)
+				}
+			} else {
+				break
+			}
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	mutex := new(sync.Mutex)
+	go TransmitData(u)
+	go ReceiveData(u, mutex)
+	w.ShowAndRun()
+}
---
+++ lab_3/rs232/serial.go
@@ -0,0 +1,194 @@
+package rs232
+
+import (
+	"errors"
+	"fmt"
+	"go.bug.st/serial"
+	"log"
+	"os"
+	"os/exec"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Port struct {
+	Name       string
+	Number     int
+	SerialPort serial.Port
+}
+
+func DefaultConfig() *serial.Mode {
+	return &serial.Mode{
+		BaudRate: 115200,
+		DataBits: 8,
+		StopBits: serial.OneStopBit,
+		Parity:   serial.NoParity,
+	}
+}
+
+func (p *Port) OpenPort(name string) (serial.Port, error) {
+	port, err := serial.Open(name, DefaultConfig())
+	if err != nil {
+		log.Printf("Port %s open failed\n", name)
+		return nil, err
+	}
+	p.Name = name
+	p.SerialPort = port
+	p.Number = extractNum(name)
+	log.Printf("Port %s opened successful\n", name)
+	return port, nil
+}
+
+func (p *Port) ClosePort() error {
+	if p.SerialPort != nil {
+		_ = p.SerialPort.ResetOutputBuffer()
+		_ = p.SerialPort.ResetInputBuffer()
+		err := p.SerialPort.Close()
+		if err != nil {
+			log.Printf("Port %s close failed\n", p.Name)
+			return err
+		}
+		log.Printf("Port %s closed successfull\n", p.Name)
+		p.SerialPort = nil
+	}
+	return nil
+}
+
+func (p *Port) WriteBytes(data []byte) error {
+	if p.SerialPort == nil {
+		return errors.New("Serial port is not open")
+	}
+	n, err := p.SerialPort.Write(data)
+	if err != nil {
+		return err
+	}
+	log.Printf("Written %d bytes to port %s\n", n, p.Name)
+	//err = p.SerialPort.ResetOutputBuffer()
+	//if err != nil {
+	//	return err
+	//}
+	return nil
+}
+
+func (p *Port) ReadBytes() ([]byte, error) {
+	if p.SerialPort == nil {
+		return nil, errors.New("Serial port is not open")
+	}
+	buff := make([]byte, 256)
+	n, err := p.SerialPort.Read(buff)
+	if err != nil {
+		return nil, err
+	}
+	//err = p.SerialPort.ResetInputBuffer()
+	//if err != nil {
+	//	return nil, err
+	//}
+	log.Printf("Read %d bytes from port %s\n", n, p.Name)
+	return buff[:n], nil
+}
+
+func (p *Port) PortNumber() (int, error) {
+	var numberStr string
+	for i := len(p.Name) - 1; i >= 0; i-- {
+		if p.Name[i] >= '0' && p.Name[i] <= '9' {
+			numberStr = string(p.Name[i]) + numberStr
+		} else {
+			break
+		}
+	}
+	if numberStr == "" {
+		return 0, errors.New("No number found in port name")
+	}
+	return strconv.Atoi(numberStr)
+}
+
+func PortIsOpen(name string) bool {
+	cmd := exec.Command("fuser", name)
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	processes := strings.Fields(string(output))
+	return len(processes) > 1
+}
+
+func PortIsOpenThisProcess(name string) bool {
+	pid := os.Getpid()
+	cmd := exec.Command("fuser", name)
+	output, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+	processes := strings.Fields(string(output))
+	for _, process := range processes {
+		if process == fmt.Sprintf("%d", pid) {
+			return true
+		}
+	}
+	return false
+}
+
+type ByNumber []string
+
+func (s ByNumber) Len() int {
+	return len(s)
+}
+
+func (s ByNumber) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s ByNumber) Less(i, j int) bool {
+	numI := extractNum(s[i])
+	numJ := extractNum(s[j])
+	return numI < numJ
+}
+
+func extractNum(s string) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] < '0' || s[i] > '9' {
+			if i == len(s)-1 {
+				return 0
+			}
+			num, err := strconv.Atoi(s[i+1:])
+			if err != nil {
+				return 0
+			}
+			return num
+		}
+	}
+	num, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+	return num
+}
+
+func RemovePorts() ([]string, error) {
+	ports, err := serial.GetPortsList()
+	sort.Sort(ByNumber(ports))
+	if err != nil {
+		return nil, err
+	}
+	if len(ports) == 0 {
+		return nil, errors.New("No serial ports found!")
+	}
+	availablePorts := make([]string, 0)
+	for i := 0; i < len(ports); i++ {
+		if !PortIsOpen(ports[i]) {
+			availablePorts = append(availablePorts, ports[i])
+		} else {
+			if PortIsOpenThisProcess(ports[i]) {
+				if i%2 == 0 {
+					i++
+				} else {
+					if !PortIsOpen(ports[i-1]) && len(availablePorts) > 0 {
+						availablePorts = availablePorts[:len(availablePorts)-1]
+					}
+				}
+			}
+		}
+	}
+	return availablePorts, nil
+}
